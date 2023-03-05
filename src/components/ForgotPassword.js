26570122cb3d0e4a7f581b21a653cb56c564c29b
boobalannaresh <bestforevermvi@gmail.com>
2023-03-05 23:20:00 +0530

    cors policy

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -20,7 +20,7 @@ const ForgotPassword = () => {
     const [message, setMessage] = useState("");
 
     const userValid = async () => {
-        const res = await fetch(`/forgotpassword/${id}/${token}`, {
+        const res = await fetch(`${env.api}/forgotpassword/${id}/${token}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
